7707ffff56d11747607c1bc234991e8e5c79f869
Sakthe-Balan <sakthebalan2003@gmail.com>
2023-06-11 07:37:43 +0530

    Final

--- final2.py
+++ final2.py
@@ -2,13 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import time
-from requests.exceptions import RequestException
+from requests.exceptions import RequestException, ChunkedEncodingError
 from urllib3.exceptions import ProtocolError
-from requests.exceptions import ChunkedEncodingError
 import re
 
-filename = 'Course_Info(Final).csv'
-fields = ['title', 'Provider', 'taught_by', 'Link', 'url', 'side_card', 'what_learn_1', 'what_learn_2','ratings','no_of_ratings']
+filename = 'Course_Info(Final-1).csv'
+fields = ['title','type', 'Provider', 'taught_by', 'Link', 'url', 'side_card', 'overview', 'ratings', 'no_of_ratings']
 
 with open(filename, 'w', newline='', encoding='UTF8') as f:
     writer = csv.writer(f)
@@ -16,7 +15,7 @@ with open(filename, 'w', newline='', encoding='UTF8') as f:
 
 for i in range(1, 661):
     # Define the URL of the webpage containing the links to be scraped
-    url = 'https://www.classcentral.com/provider/udemy?page=' + str(i)
+    url = 'https://www.classcentral.com/institution/smithsonian?page=' + str(i)
 
     # Define the headers to be used for making requests to the website
     headers = {
@@ -58,67 +57,80 @@ for i in range(1, 661):
 
                 # Scrape the data inside the link using BeautifulSoup
                 try:
-                    title = link_soup2.find('h1', attrs={'class': 'head-2'}).get_text().strip()
+                    title = link_soup2.find('h1', attrs={'class': 'medium-up-head-1'}).get_text().strip()
                 except AttributeError:
-                    print("Title not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    print("Title not available. Leaving it blank and continuing...")
+                    title = ''
 
+                try:
+                    type = link_soup2.find('div', attrs={'class': 'small-down-margin-top-small margin-bottom-xsmall medium-up-margin-bottom-small'}).get_text().strip()
+                    type = '/'.join(type.split())
+                except AttributeError:
+                    print("Type not available. Leaving it blank and continuing...")
+                    type = ''
+
+                dummy=0
                 try:
                     ratings = link_soup2.find('p', attrs={'class': 'text-2 medium-down-margin-top-xsmall large-up-margin-left-xsmall inline-block'}).get_text().strip()
                     ratings = ratings.replace('\n', ' ').replace('\r', '')  # Replace line breaks with spaces
                     ratings = re.sub(' +', ' ', ratings)  # Replace multiple spaces with a single space
                 except AttributeError:
-                    print("Title not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    try:
+                        p_element = link_soup2.find('p', class_='text-1 medium-down-margin-top-xsmall large-up-margin-left-xsmall inline-block')
+                        strong_elements = []
+                        if p_element:
+                            strong_elements = p_element.find_all('strong', class_='weight-bold')
 
-                # Extract the rating and number of ratings using regular expressions
-                rating_match = re.search(r'(\d+(\.\d+)?) rating', ratings)
-                number_of_ratings_match = re.search(r'based on (\d+) ratings', ratings)
+                        # Extract the text content from the first two <strong> elements
+                        rating = strong_elements[0].get_text() if len(strong_elements) > 0 else ''
+                        rating= ' '.join(rating.split())
+                        number_of_ratings = strong_elements[1].get_text() if len(strong_elements) > 1 else ''
+                        number_of_ratings= '/'.join(number_of_ratings.split())
+                        dummy=1
 
-                # Assign the extracted values to separate variables
-                rating = float(rating_match.group(1))
-                number_of_ratings = int(number_of_ratings_match.group(1))
+                       
+                    except AttributeError:
+                        print("Ratings not available. Leaving it blank and continuing...")
+                        rating = ''
+                        number_of_ratings=''
+                
+                if(dummy==0):
+                    # Extract the rating and number of ratings using regular expressions
+                    rating_match = re.search(r'(\d+(\.\d+)?) rating', ratings)
+                    number_of_ratings_match = re.search(r'based on (\d+) ratings', ratings)
+
+                    # Assign the extracted values to separate variables
+                    rating = float(rating_match.group(1)) if rating_match else ''
+                    number_of_ratings = int(number_of_ratings_match.group(1)) if number_of_ratings_match else ''
 
 
                 try:
                     provider = link_soup2.find('a', attrs={'class': 'text-1 link-gray-underline'}).get_text().strip()
                 except AttributeError:
-                    print("Provider not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    print("Provider not available. Leaving it blank and continuing...")
+                    provider = ''
 
                 try:
                     h3_with_class = link_soup2.find('div', class_='course-noncollapsable-section')
                     p_with_class = h3_with_class.find('p', class_='text-1')
                     taught_by = p_with_class.get_text().strip()
                 except AttributeError:
-                    print("Taught by not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    print("Taught by not available. Leaving it blank and continuing...")
+                    taught_by = ''
 
                 try:
                     link = link_soup2.find('a', attrs={'class': 'btn-blue btn-medium width-100 padding-horz-xlarge'}).get('href').strip()
                     link = 'https://www.classcentral.com' + link
                 except AttributeError:
-                    print("Link not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    print("Link not available. Leaving it blank and continuing...")
+                    link = ''
 
                 try:
                     a_element = link_soup2.find('a', class_='text-1 link-gray-underline')
                     url = a_element['href']
                 except AttributeError:
-                    print("URL not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    print("URL not available. Leaving it blank and continuing...")
+                    url = ''
 
                 try:
                     li_list = link_soup2.find_all('li', class_='course-details-item border-gray-light')
@@ -128,70 +140,51 @@ for i in range(1, 661):
                         span_with_class = li_with_class.find('span', class_='text-2 line-tight')
                         if span_with_class:
                             side_card = span_with_class.get_text().strip()
+                            # Limit the maximum number of gaps between words to 1
+                            side_card = ' '.join(side_card.split())
                             side_cards.append(side_card)
                         else:
                             a_with_class = li_with_class.find('a', class_='text-2 color-charcoal line-tight')
                             if a_with_class:
                                 side_card = a_with_class.get_text().strip()
+                                # Limit the maximum number of gaps between words to 1
+                                side_card = ' '.join(side_card.split())
                                 side_cards.append(side_card)
 
                 except AttributeError:
-                    print("side_card not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    print("Side card not available. Leaving it blank and continuing...")
+                    side_cards = []
 
                 try:
                     content_element = link_soup2.find('div', class_='truncatable-area')
                     if content_element is None:
                         content_element = link_soup2.find('div', class_='wysiwyg text-1 line-wide')
 
-                    # Extract the text from the element
-                    what_learn_1 = content_element.get_text(strip=True)
-                    separator = "What you'll learn:"
-                    if separator in what_learn_1:
-                        what_learn_1 = what_learn_1.split(separator, 1)[0].strip()
-
-                except AttributeError:
-                    print("What you'll learn-1 not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
-
-                try:
-                    div_element = link_soup2.find('div', class_='truncatable-area')
-                    if div_element is None:
-                        div_element = link_soup2.find('div', class_='wysiwyg text-1 line-wide')
-
-                    # Remove attributes from the initial div element
-                    div_element.attrs = {}
-
-                    # Find the separator string
-                    separator = "What you'll learn:"
-                    separator_index = div_element.get_text().find(separator)
-
-                    if separator_index != -1:
-                        # Extract the portion of HTML after the separator
-                        what_learn_2 = str(div_element)[separator_index + len(separator):].strip()
-
+                    # Extract the raw HTML content from the element
+                    if content_element:
+                        overview = content_element.prettify()
                     else:
-                        print("What you'll learn-2 not available. Retrying...")
-                        time.sleep(2)
-                        retry_count += 1
-                        continue
+                        overview = ''
 
                 except AttributeError:
-                    print("What you'll learn-2 not available. Retrying...")
-                    time.sleep(2)
-                    retry_count += 1
-                    continue
+                    print("Overview not available. Leaving it blank and continuing...")
+                    overview = ''
 
-                # Write the data to the CSV file
-                with open(filename, 'a+', newline='', encoding='UTF8') as f:
+
+
+                
+
+                # Write the scraped data to the CSV file
+                with open(filename, 'a', newline='', encoding='UTF8') as f:
                     writer = csv.writer(f)
-                    writer.writerow([title,rating,number_of_ratings, provider, taught_by, link, url, side_cards, what_learn_1, what_learn_2])
+                    writer.writerow([title,type, provider, taught_by, link, url, side_cards, overview, rating, number_of_ratings])
+
                 break
-            except AttributeError:
-                print(f"Data not available. Retrying in 2 seconds... Retry count: {retry_count}")
-                time.sleep(2)
+            except (RequestException, ChunkedEncodingError, ProtocolError) as e:
+                print(f"Error: {e}")
                 retry_count += 1
+                print(f"Retrying... Retry count: {retry_count}")
+                time.sleep(2)  # wait for 2 seconds before retrying
+                continue
+        else:
+            print("Failed to retrieve data for the link. Moving on to the next link...")
